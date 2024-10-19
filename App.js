3e8717eb2be940145aae440d403702a899105fcc
Rayan Diniz <rayabncassiokiol@gmail.com>
2024-10-19 00:50:51 -0300

    atualizaçao

--- App.js
+++ App.js
@@ -7,144 +7,143 @@ const shuffleArray = (array) => {
 };
 
 // Configurando retorno do objeto
-let requestURL = "./data.json";
-let request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
+let requestURL = "./data.json"
+let request = new XMLHttpRequest()
+request.open("GET", requestURL)
+request.responseType = "json"
+request.send()
 
 request.onload = function() {
-  let quests = request.response;
-  showContent(quests);
+  let quests = request.response
+  showContent(quests)
   //console.log(quests["quests"])
-};
+}
 //----
 
-let currentQuestionIndex = 0;
-let section = document.querySelector("section");
-section.setAttribute("class", "main");
+let currentQuestionIndex = 0
+let section = document.querySelector("section")
+section.setAttribute("class", "main")
 
 const showContent = (jsonObj) => {
-  let content = jsonObj["quests"];
+  let content = jsonObj["quests"]
   
   // Embaralhar questões
- // shuffleArray(content);
+ // shuffleArray(content)
 
   content.forEach((question, index) => {
     // Embaralhar opções
-    shuffleArray(question.options);
+    shuffleArray(question.options)
 
-    let myDivQuest = document.createElement("div");
-    myDivQuest.setAttribute("class", "question hidden");
-    myDivQuest.setAttribute("id", "question" + index);
+    let myDivQuest = document.createElement("div")
+    myDivQuest.setAttribute("class", "question hidden")
+    myDivQuest.setAttribute("id", "question" + index)
 
-    let mySpan = document.createElement("span");
-    mySpan.setAttribute("id", "result" + (index + 1));
+    let mySpan = document.createElement("span")
+    mySpan.setAttribute("id", "result" + (index + 1))
 
-    let myH2 = document.createElement("h2");
-    myH2.textContent = (index + 1) + ". " + question.quest;
-    myH2.appendChild(mySpan);
-    myDivQuest.appendChild(myH2);
+    let myH2 = document.createElement("h2")
+    myH2.textContent = (index + 1) + ". " + question.quest
+    myH2.appendChild(mySpan)
+    myDivQuest.appendChild(myH2)
 
     if (question.img != "") {
-      let myImg = document.createElement("img");
-      myImg.setAttribute("src", question.img);
-      myDivQuest.appendChild(myImg);
+      let myImg = document.createElement("img")
+      myImg.setAttribute("src", question.img)
+      myDivQuest.appendChild(myImg)
     }
 
     question.options.forEach((option, j) => {
-      let myDivOption = document.createElement("div");
-      let myInput = document.createElement("input");
-      let myLabel = document.createElement("label");
+      let myDivOption = document.createElement("div")
+      let myInput = document.createElement("input")
+      let myLabel = document.createElement("label")
 
-      myDivOption.setAttribute("class", "myDivOption");
+      myDivOption.setAttribute("class", "myDivOption")
 
-      myInput.setAttribute("type", "radio");
-      myInput.setAttribute("name", "option" + index);
-      myInput.setAttribute("value", option);
-      myInput.setAttribute("id", "option" + index + "_" + j);
+      myInput.setAttribute("type", "radio")
+      myInput.setAttribute("name", "option" + index)
+      myInput.setAttribute("value", option)
+      myInput.setAttribute("id", "option" + index + "_" + j)
 
-      myLabel.setAttribute("for", "option" + index + "_" + j);
-      myLabel.textContent = option;
+      myLabel.setAttribute("for", "option" + index + "_" + j)
+      myLabel.textContent = option
 
-      myDivOption.appendChild(myInput);
-      myDivOption.appendChild(myLabel);
-      myDivQuest.appendChild(myDivOption);
-    });
+      myDivOption.appendChild(myInput)
+      myDivOption.appendChild(myLabel)
+      myDivQuest.appendChild(myDivOption)
+    })
 
-    section.appendChild(myDivQuest);
-  });
+    section.appendChild(myDivQuest)
+  })
 
-  document.getElementById("question0").classList.remove("hidden");
-};
+  document.getElementById("question0").classList.remove("hidden")
+}
 
-let clicked = false;
-
+let clicked = false
 const showResult = () => {
   if (!clicked) {
-    let quests = request.response.quests;
-    let score = 0;
-    let totalQuestions = quests.length; 
+    let quests = request.response.quests
+    let score = 0
+    let totalQuestions = quests.length 
     
     for (let i = 0; i < quests.length; i++) {
-      let options = document.getElementsByName("option" + i);
-      let result = document.getElementById("result" + (i + 1));
-      let correctAnswer = quests[i].correct; // Supondo que o objeto JSON tenha uma propriedade 'correct'
+      let options = document.getElementsByName("option" + i)
+      let result = document.getElementById("result" + (i + 1))
+      let correctAnswer = quests[i].correct // Supondo que o objeto JSON tenha uma propriedade 'correct'
       console.log(correctAnswer)
 
       for (let option of options) {
         if (option.checked) {
           if (option.value === correctAnswer) {
-            score++;
-            result.innerText = "Correto";
-            result.style.color = "green";
+            score++
+            result.innerText = "Correto"
+            result.style.color = "green"
           } else {
-            result.innerText = "Errado";
-            result.style.color = "red";
+            result.innerText = "Errado"
+            result.style.color = "red"
           }
         }
       }
     }
     
-    let resultSection = document.createElement("div");
-    resultSection.setAttribute("id", "resultSection");
-    resultSection.textContent = `Você acertou ${score} de ${totalQuestions} questões.`;
-    section.appendChild(resultSection);
-    clicked = true;
+    let resultSection = document.createElement("div")
+    resultSection.setAttribute("id", "resultSection")
+    resultSection.textContent = `Você acertou ${score} de ${totalQuestions} questões.`
+    section.appendChild(resultSection)
+    clicked = true
   }
-};
+}
 
 const nextQuestion = () => {
-  let totalQuestions = document.getElementsByClassName("question").length;
+  let totalQuestions = document.getElementsByClassName("question").length
   if (currentQuestionIndex < totalQuestions - 1) {
-    document.getElementById("question" + currentQuestionIndex).classList.add("hidden");
-    currentQuestionIndex++;
-    document.getElementById("question" + currentQuestionIndex).classList.remove("hidden");
+    document.getElementById("question" + currentQuestionIndex).classList.add("hidden")
+    currentQuestionIndex++
+    document.getElementById("question" + currentQuestionIndex).classList.remove("hidden")
   }
 
   if (currentQuestionIndex === totalQuestions - 1) {
-    document.getElementById("nextBtn").classList.add("hidden");
-    document.getElementById("finishBtn").classList.remove("hidden");
+    document.getElementById("nextBtn").classList.add("hidden")
+    document.getElementById("finishBtn").classList.remove("hidden")
   }
 
   if (currentQuestionIndex > 0) {
-    document.getElementById("prevBtn").classList.remove("hidden");
+    document.getElementById("prevBtn").classList.remove("hidden")
   }
-};
+}
 
 const prevQuestion = () => {
   if (currentQuestionIndex > 0) {
-    document.getElementById("question" + currentQuestionIndex).classList.add("hidden");
-    currentQuestionIndex--;
-    document.getElementById("question" + currentQuestionIndex).classList.remove("hidden");
+    document.getElementById("question" + currentQuestionIndex).classList.add("hidden")
+    currentQuestionIndex--
+    document.getElementById("question" + currentQuestionIndex).classList.remove("hidden")
   }
 
   if (currentQuestionIndex === 0) {
-    document.getElementById("prevBtn").classList.add("hidden");
+    document.getElementById("prevBtn").classList.add("hidden")
   }
 
   if (currentQuestionIndex < document.getElementsByClassName("question").length - 1) {
-    document.getElementById("nextBtn").classList.remove("hidden");
-    document.getElementById("finishBtn").classList.add("hidden");
+    document.getElementById("nextBtn").classList.remove("hidden")
+    document.getElementById("finishBtn").classList.add("hidden")
   }
 };
