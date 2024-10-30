53488c3f002dc9a5f1cf659cbc94126b826dff7c
Rayan Diniz <rayabncassiokiol@gmail.com>
2024-10-29 21:34:37 -0300

    ajustes

--- app/App.js
+++ app/App.js
@@ -1,20 +1,40 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
+import { AdMobBanner } from "expo";
 
 export default function App() {
   return (
-    <WebView
-      style={styles.container}
-      source={{ uri: 'https://simuladosautoescola.netlify.app/' }}
-    />
+    <View style={styles.main} >
+      {/* <AdMobBanner
+        style={styles.topBanner}
+        bannerSize="fullBanner"
+        adUnitID="ca-app-pub-4249431492821525/9379750934"
+        // Test ID, Replace with your-admob-unit-id
+        testDeviceID="EMULATOR"
+        didFailToReceiveAdWithError={this.bannerError}
+      /> */}
+
+      <WebView
+        style={styles.container}
+        source={{ uri: 'https://simuladosautoescola.netlify.app/' }}
+      />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  topBanner: {
+  position: "absolute",
+  top: 0
+  },
+  main: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    height: "100%"
+  }
 });
