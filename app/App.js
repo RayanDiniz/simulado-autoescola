4eaa381a52a645ea2b3cb5711b34ef7341a18518
Rayan Diniz <rayabncassiokiol@gmail.com>
2024-10-31 22:02:11 -0300

    novas questões adicionadas

--- app/App.js
+++ app/App.js
@@ -5,19 +5,27 @@ import { AdMobBanner } from "expo";
 
 export default function App() {
   return (
-    
-
+    <View style={styles.main} >
+      <AdMobBanner
+        style={styles.topBanner}
+        bannerSize="fullBanner"
+        adUnitID="ca-app-pub-4249431492821525/9379750934"
+        // Test ID, Replace with your-admob-unit-id
+        testDeviceID="EMULATOR"
+        didFailToReceiveAdWithError={this.bannerError}
+      />
       <WebView
         style={styles.container}
         source={{ uri: 'https://simuladosautoescola.netlify.app/' }}
       />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   topBanner: {
-  position: "absolute",
-  top: 0
+    position: "absolute",
+    top: 0
   },
   main: {
     flex: 1,
